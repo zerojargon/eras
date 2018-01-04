04bc1d2e07ec06e50511300627dc06086255efbe
Sam Cook <samryancook@gmail.com>
2018-01-04 20:55:16 +0000

    Enhancements and fixes

--- src/components/organisms/SEO/ContactForm/ContactForm.jsx
+++ src/components/organisms/SEO/ContactForm/ContactForm.jsx
@@ -2,18 +2,59 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { reduxForm, Field } from 'redux-form'
-import { Button, Glyphicon } from 'react-bootstrap'
+import { Button, Glyphicon, FormGroup, ControlLabel } from 'react-bootstrap'
 import { Input } from '../../../atoms'
 
 class ContactForm extends Component {
   render() {
     return (
       <form onSubmit={this.props.handleSubmit(this.props.onSubmit)}>
-        <Field component={Input} type="text" name="subject" placeholder="Subject"/>
-        <Field component={Input} type="text" name="productId" placeholder="Product Code"/>
-        <Field component={Input} type="email" name="email" placeholder="Your Email Address" required/>
-        <Field component={Input} type="number" name="phone" placeholder="Your Telephone Number"/>
-        <Field component={Input} type="textarea" name="message" placeholder="Your Message"/>
+        <Field
+          component={Input}
+          type="text"
+          name="subject"
+          id="subject"
+          placeholder="Subject"
+          label="Subject:"
+        />
+
+        <Field
+          component={Input}
+          type="text"
+          name="productId"
+          id="productId"
+          placeholder="Product Code / Name"
+          label="Product Code / Name:"
+        />
+
+        <Field
+          component={Input}
+          type="email"
+          name="email"
+          id="email"
+          placeholder="Your Email Address"
+          label="Email:"
+          required
+        />
+
+        <Field
+          component={Input}
+          type="number"
+          name="phone"
+          id="phone"
+          placeholder="Your Telephone Number"
+          label="Telephone:"
+        />
+
+        <Field
+          component={Input}
+          type="textarea"
+          name="message"
+          id="message"
+          placeholder="Your Message"
+          label="Message:"
+        />
+        
         <Button type="submit">Submit <Glyphicon glyph="send" /></Button>
       </form>
     )
